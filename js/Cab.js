0d76781b0c01d849f7a47c2042d1dc18cb7d6b96
PedroMessias101 <pedromessias101@gmail.com>
2025-05-06 10:53:03 -0300

    finalizando a pagina de Editar perfil

--- js/Cab.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('.profile').addEventListener('mouseover', function() {
-    this.querySelector('.dropdown').style.display = 'block';
-});
-
-document.querySelector('.profile').addEventListener('mouseout', function() {
-    this.querySelector('.dropdown').style.display = 'none';
-});
--- js/estrela.js
+++ js/estrela.js
@@ -1,5 +1,8 @@
 const star = document.querySelector(".star");
 const starFill = document.querySelector(".starFill");
+const temaEsc = document.getElementById("temaEsc");
+
+let isDarkMode = false; // Variável para rastrear o estado do tema
 
 function fill() {
   if (star.style.display === "none") {
@@ -8,6 +11,19 @@ function fill() {
   } else {
     star.style.display = "none";
     starFill.style.display = "block";
-
   }
 }
+
+temaEsc.addEventListener("click", () => {
+  if (isDarkMode) {
+    // Voltar para o tema claro
+    document.body.style.backgroundColor = "#ffffff"; // Cor de fundo clara
+    document.body.style.color = "#000000"; // Texto escuro
+    isDarkMode = false;
+  } else {
+    // Alterar para o tema escuro
+    document.body.style.backgroundColor = "#747474"; // Cor de fundo escura
+    document.body.style.color = "#ffffff"; // Texto claro
+    isDarkMode = true;
+  }
+});
