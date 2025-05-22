3dcf034757566490a9a7ff1221b1a30ee03c9c7a
Gabrielyfs <felixdesouzagabriely@gmail.com>
2025-05-22 16:44:09 -0300

    cadastro realizado

---
+++ prisma/migrations/20250522165825_atualizando_date/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `user` MODIFY `nascimento` DATE NOT NULL;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,12 +4,12 @@ const { PrismaClient } = require('@prisma/client')
 const router = express.Router()
 const prisma = new PrismaClient()
 
-// Criar usuario
+// Criar user
 router.post("/cadastro", async (req, res) => {
     // recebe os dados do corpo da requisição
     const { nome, email, telefone, nascimento, senha } = req.body
-
-    const userExist = await prisma.usuario.findUnique({
+    console.log(nome, email, telefone, nascimento, senha)
+    const userExist = await prisma.user.findFirst({
         where: { email }
     })
 
@@ -17,13 +17,13 @@ router.post("/cadastro", async (req, res) => {
         return res.status(400).json({ error: "E-mail já cadastrado" })
     }
 
-    // Criar usuario
-    const user = await prisma.usuario.create({
+    // Criar user
+    const user = await prisma.user.create({
         data: {
             nome,
             email,
-            telefone,
-            nascimento,
+            telefone: parseInt(telefone),
+            nascimento: new Date(nascimento),
             senha
         }
     })
@@ -35,13 +35,13 @@ router.put("/cadastro/:id", async (req, res) => {
     const { id } = req.params
     const { nome, senha } = req.body
 
-    const user = await prisma.usuario.findUnique({ where: { id: Number(id) } })
+    const user = await prisma.user.findUnique({ where: { id: Number(id) } })
 
     if (!user) {
-        return res.status(404).json({ error: "Usuario não encontrado" })
+        return res.status(404).json({ error: "user não encontrado" })
     }
 
-    const updateUser = await prisma.usuario.update({
+    const updateUser = await prisma.user.update({
         where: { id: Number(id) },
         data: {
             nome,
@@ -53,10 +53,10 @@ router.put("/cadastro/:id", async (req, res) => {
 
 })
 
-router.post("/login", async (req, res) => {
+router.post("/", async (req, res) => {
     const { email, senha } = req.body
 
-    const user = await prisma.usuario.findUnique({
+    const user = await prisma.user.findUnique({
         where: { email }
     })
 
