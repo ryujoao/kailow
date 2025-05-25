c16352ffd557856353947c4a329d494f7c71707d
Gabrielyfs <felixdesouzagabriely@gmail.com>
2025-05-25 01:01:39 -0300

    Adicionando as validações do cadastro e login

--- prisma/migrations/20250524204217_kailow/migration.sql
+++ prisma/migrations/20250524204217_kailow/migration.sql
@@ -4,7 +4,16 @@ CREATE TABLE `User` (
     `nome` VARCHAR(191) NOT NULL,
     `email` VARCHAR(191) NOT NULL,
     `telefone` INTEGER NOT NULL,
-    `nascimento` DATETIME(3) NOT NULL,
+    `nascimento` DATE NOT NULL,
+    `senha` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Login` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(191) NOT NULL,
     `senha` VARCHAR(191) NOT NULL,
 
     PRIMARY KEY (`id`)
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,17 +4,26 @@ const { PrismaClient } = require('@prisma/client')
 const router = express.Router()
 const prisma = new PrismaClient()
 
-// Criar user
+// Criar user -> Cadastro
 router.post("/cadastro", async (req, res) => {
     // recebe os dados do corpo da requisição
     const { nome, email, telefone, nascimento, senha } = req.body
     console.log(nome, email, telefone, nascimento, senha)
-    const userExist = await prisma.user.findFirst({
+
+    const userExistEmail = await prisma.user.findUnique({
         where: { email }
     })
 
-    if (userExist) {
-        return res.status(400).json({ error: "E-mail já cadastrado" })
+    if (userExistEmail) {
+        return res.status(400).json({ error: "*E-mail já cadastrado" })
+    }
+
+    const userExistTelefone = await prisma.user.findUnique({
+        where: { telefone }
+    })
+
+    if (userExistTelefone) {
+        return res.status(400).json({ error: "*Telefone já cadastrado" })
     }
 
     // Criar user
@@ -22,7 +31,7 @@ router.post("/cadastro", async (req, res) => {
         data: {
             nome,
             email,
-            telefone: parseInt(telefone),
+            telefone,
             nascimento: new Date(nascimento),
             senha
         }
@@ -31,6 +40,8 @@ router.post("/cadastro", async (req, res) => {
     return res.status(201).json(user)
 })
 
+
+
 router.put("/cadastro/:id", async (req, res) => {
     const { id } = req.params
     const { nome, senha } = req.body
@@ -38,7 +49,7 @@ router.put("/cadastro/:id", async (req, res) => {
     const user = await prisma.user.findUnique({ where: { id: Number(id) } })
 
     if (!user) {
-        return res.status(404).json({ error: "user não encontrado" })
+        return res.status(404).json({ error: "*Usuário não encontrado" })
     }
 
     const updateUser = await prisma.user.update({
@@ -53,6 +64,8 @@ router.put("/cadastro/:id", async (req, res) => {
 
 })
 
+
+// Login
 router.post("/", async (req, res) => {
     const { email, senha } = req.body
 
@@ -60,12 +73,8 @@ router.post("/", async (req, res) => {
         where: { email }
     })
 
-    if (!user) {
-        return res.status(401).json({ error: "E-mail ou senha incorretos" })
-    }
-
-    if (senha != user.senha) {
-        return res.status(400).json({ error: "E-mail ou senha incorretos" })
+    if (!user || senha != user.senha) {
+        return res.status(401).json({ error: "*E-mail ou senha incorretos" })
     }
 
     return res.json({ message: "Login bem sucedido" })
